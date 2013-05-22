752f3100adcc84b09cd2708640fb6470eb5a5d19
Calvin De Lima <calvindlm@gmail.com>
2013-05-22 06:02:08 -0300

    Update settings.py

--- tocloud_app/settings.py
+++ tocloud_app/settings.py
@@ -10,18 +10,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tocloud_app',                      # Or path to database file if using sqlite3.
-        'USER': 'tocloud_app',
-        'PASSWORD': 't2heFuna',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
